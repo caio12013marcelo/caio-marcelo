169c0adb0f57788eb1cfa2a684df981b27f0e24a
Cairo <caio12013marcelo@gmail.com>
2026-08-04 21:09:37 -0300

    Update Ex06.java

--- Tarefas/src/Ex06.java
+++ Tarefas/src/Ex06.java
@@ -2,8 +2,32 @@ import java.util.Scanner;
 
 public class Ex06 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        
+        int[][] matriz = {
+                {210, 3, 399900},
+                {160, 3, 329900},
+                {240, 3, 369000},
+                {141, 2, 232000},
+                {300, 4, 539900},
+                {198, 4, 299900},
+                {153, 3, 314900},
+                {142, 3, 199000},
+                {138, 3, 212000},
+                {149, 3, 242500},
+                {194, 4, 240000},
+                {200, 3, 347000},
+                {189, 3, 330000},
+                {447, 5, 699900},
+                {126, 3, 259900}
+        };
+        int somaValor=0;
+        for (int i=0;i<matriz.length;i++){
+            for (int j=0;j<matriz.length;j++){
+                if(j == 2){
+                    somaValor= somaValor+matriz[i][j];
+                }
+            }
+        }
+        System.out.println(somaValor);
     }
 }
