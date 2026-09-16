46cd3df3200e89c4748524e0323fe267cd3ac6d9
Cairo <caio12013marcelo@gmail.com>
2026-09-15 21:46:35 -0300

    Update Aeronave.java

--- Atividades/Atividade 01/Ex07/src/Aeronave.java
+++ Atividades/Atividade 01/Ex07/src/Aeronave.java
@@ -1,9 +1,9 @@
 public class Aeronave {
-    String modelo;
-    int pasageiros;
-    double velocidadeMax;
-    double capacidadeCombustivel;
-    double combustivelPorMinuto;
+    private String modelo;
+    private int pasageiros;
+    private double velocidadeMax;
+    private double capacidadeCombustivel;
+    private double combustivelPorMinuto;
 
     public Aeronave(String modelo, int pasageiros, double velocidadeMax, double capacidadeCombustivel, double combustivelPorMinuto) {
         this.modelo = modelo;
@@ -17,6 +17,46 @@ public class Aeronave {
     }
     public double calcularLongividadeVooMax(){
         return calcularTempoDeVoo()/velocidadeMax;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPasageiros() {
+        return pasageiros;
+    }
+
+    public void setPasageiros(int pasageiros) {
+        this.pasageiros = pasageiros;
+    }
+
+    public double getVelocidadeMax() {
+        return velocidadeMax;
+    }
+
+    public void setVelocidadeMax(double velocidadeMax) {
+        this.velocidadeMax = velocidadeMax;
+    }
+
+    public double getCapacidadeCombustivel() {
+        return capacidadeCombustivel;
+    }
+
+    public void setCapacidadeCombustivel(double capacidadeCombustivel) {
+        this.capacidadeCombustivel = capacidadeCombustivel;
+    }
+
+    public double getCombustivelPorMinuto() {
+        return combustivelPorMinuto;
+    }
+
+    public void setCombustivelPorMinuto(double combustivelPorMinuto) {
+        this.combustivelPorMinuto = combustivelPorMinuto;
     }
 
     @Override
